721551da6c66faeec40360a5816be3433710ac9d
Jesús Christian Castillo <christian.castillo.lozano@gmail.com>
2020-01-14 17:31:44 -0600

    Mi Primer componente

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+//funcion estandar en React
+function Header (){
+
+    //Antes del return puedes escribir variables para utilizar dentro del return
+    const age = 15; 
+    let message='';
+    if(age>=18){
+        message='Eres mayor de edad';
+    }else{
+        message='Eres menor de edad';
+    }
+
+    return(
+        //Para declarar variables utiliza  {variable}
+    <h1>{message}</h1>
+        
+    )
+}
+
+
+export default Header;
