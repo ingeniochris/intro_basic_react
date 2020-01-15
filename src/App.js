24c283dd4e91d4689b7fead0b0541fba01bb7306
Jesús Christian Castillo <christian.castillo.lozano@gmail.com>
2020-01-14 18:33:46 -0600

    Utilizando los Props usando Destructuring en lugar de props.title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React, {Fragment} from 'react'; // Con Fragment puedes encerrar los elementos / componentes
+import React, {Fragment, useState} from 'react'; // Con Fragment puedes encerrar los componentes !!! Agregamos useState y usas Hooks!!!!!!
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 //Componente clasico "Funtion Declaration" ES5
 function App() {
+
+  //crear listado de productos usando Hooks__ Aplicamos Destructuring para crear los funciones a usas ejemplo products "state original" y getProducts "agrega o elimina---state modificado"
+  const [products, getProducts] = useState([
+    {id: 1, name: 'JavaScrip', price: 50},
+    {id: 1, name: 'Angular', price: 50},
+    {id: 1, name: 'Vue', price: 50},
+    {id: 1, name: 'Svelte', price: 50},
+  ]);
+
 
 //Obtener fecha y la mandamos como props a footer
 const date = new Date().getFullYear();
