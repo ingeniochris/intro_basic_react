ec52071779a4b8d16e4512f50c0dd41fc1f6844f
Jesús Christian Castillo <christian.castillo.lozano@gmail.com>
2020-01-14 18:53:49 -0600

    Mostrar colleccion con el state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React, {Fragment, useState} from 'react'; // Con Fragment puedes encerrar los componentes !!! Agregamos useState y usas Hooks!!!!!!
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React, { Fragment, useState } from "react"; // Con Fragment puedes encerrar los componentes !!! Agregamos useState y usas Hooks!!!!!!
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Product from "./components/Product";
 
 //Componente clasico "Funtion Declaration" ES5
 function App() {
-
   //crear listado de productos usando Hooks__ Aplicamos Destructuring para crear los funciones a usas ejemplo products "state original" y getProducts "agrega o elimina---state modificado"
   const [products, getProducts] = useState([
-    {id: 1, name: 'JavaScrip', price: 50},
-    {id: 1, name: 'Angular', price: 50},
-    {id: 1, name: 'Vue', price: 50},
-    {id: 1, name: 'Svelte', price: 50},
+    { id: 1, name: "JavaScrip", price: 50 },
+    { id: 2, name: "Angular", price: 50 },
+    { id: 3, name: "Vue", price: 50 },
+    { id: 4, name: "Svelte", price: 50 }
   ]);
 
-
-//Obtener fecha y la mandamos como props a footer
-const date = new Date().getFullYear();
+  //Obtener fecha y la mandamos como props a footer
+  const date = new Date().getFullYear();
 
   return (
     <Fragment>
-      <Header title='Introduccion a React' />
-      <Footer date={date}/>
+      <Header title="Introduccion a React" />
+      <h1>Lista de productos</h1>
+      {products.map(product => (
+          <Product 
+            key={product.id}
+            product={product} 
+          />
+      ))}
+      <Footer 
+        date={date} 
+      />
     </Fragment>
   );
 }
